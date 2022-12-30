1a3e1e362e807ec505cd6bdcb0652d06d0d06dd7
Mitchell Nwafor <cozymitch2624@gmail.com>
2022-12-30 13:40:36 +0000

    updated task 4 bc of betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The starting point
+ *
+ * Return: Always 0 and is void.
+ */
+
 int main(void)
 {
-	puts("Programming is like a multilingual puzzle");
+	puts("\"Programming is like a multilingual puzzle");
 
 	return (0);
 }
